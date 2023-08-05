d148c17204d2f8418dcbd6d6a875acf97f1822de
andiq123 <andy.u789@gmail.com>
2023-08-06 00:15:01 +0300

    fix

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -1,23 +1,23 @@
-"use client";
-
 import Link from "next/link";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 type Props = {};
 
 export default function Intro({}: Props) {
-  const width = window.innerWidth >= 768 ? 500 : 300;
-
   return (
     <div className="hero py-52 slide-up" id="intro">
       <div className="hero-content flex-col lg:flex-row-reverse text-center -mt-28 bg-base-200 bg-opacity-20 backdrop-blur-lg">
-        <Image
-          src="/cardealer.jpg"
-          width={width}
-          height={500}
-          alt="dealer"
-          className="max-w-xl rounded-xl shadow-2xl bg-cover"
-        />
+        <div className="w-full">
+          <Image
+            src="/cardealer.jpg"
+            width={0}
+            height={0}
+            alt="dealer"
+            sizes="100vw"
+            className="max-w-xl rounded-xl shadow-2xl bg-cover w-full h-full"
+          />
+        </div>
         <div className="">
           <h1 className="text-5xl font-bold">Automobili di Marian Bucurel</h1>
           <p className="py-6">
